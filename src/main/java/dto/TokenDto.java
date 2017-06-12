46d6df2cc6cf10da49ec200514420c7d5c473982
felipemoura <felipemouracv@gmail.com>
2017-06-12 20:28:43 -0300

    Token DTO

---
+++ src/main/java/dto/TokenDto.java
@@ -0,0 +1,18 @@
+package dto;
+
+/**
+ * Created by felipemoura on 07/06/2017.
+ */
+public class TokenDto {
+    
+    private String token;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+}
