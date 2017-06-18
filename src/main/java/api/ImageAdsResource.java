c432d69378fca74e6750ac3c91f40cc7562262c5
felipemoura <felipemouracv@gmail.com>
2017-06-18 00:31:20 -0300

    ImagensAds recebe um Array de Images e injeta num dto para responder

--- src/main/java/api/ImageAdsResource.java
+++ src/main/java/api/ImageAdsResource.java
@@ -1,25 +1,15 @@
 package api;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.LinkOption;
-import java.util.List;
-import java.util.Map;
-
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import entity.ImageAds;
 import helper.UploadHelper;
-import org.apache.commons.io.IOUtils;
-import org.jboss.resteasy.plugins.providers.multipart.InputPart;
 import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInput;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Path("image-ads")
 public class ImageAdsResource {
@@ -29,13 +19,25 @@ public class ImageAdsResource {
     @POST
     @Path("/{id}")
     @Consumes("multipart/form-data")
+    @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
     public Response uploadFile(@PathParam("id") Integer id, MultipartFormDataInput dataInput) {
-
-        String file = UploadHelper.updateArchive(dataInput,UPLOADED_FILE_PATH );
-        if(file==null){
-            return Response.status(400).entity(file).build();
+        try {
+            ArrayList file = UploadHelper.updateArchive(dataInput,UPLOADED_FILE_PATH );
+            if(file==null){
+                return Response.status(400).entity(file).build();
+            }
+            List<ImageAds> imageAds = new ArrayList<>();
+            for (Object f : file){
+                System.out.print(f);
+                ImageAds imageInsert = new ImageAds();
+                imageInsert.setAdsId(id);
+                imageInsert.setName(f.toString());
+                imageAds.add(imageInsert);
+            }
+            return Response.status(200).entity(imageAds).build();
+        } catch (Exception e){
+            return Response.status(400).entity(e.getMessage()).build();
         }
-        return Response.status(200).entity(file).build();
     }
 
 }
