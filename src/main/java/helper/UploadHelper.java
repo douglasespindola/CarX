9bdb60589414c3ef84bfc55f352ed09bab1f6166
felipemoura <felipemouracv@gmail.com>
2017-06-18 00:40:28 -0300

    todas as letras lowercase e espaços trocados por _

--- src/main/java/helper/UploadHelper.java
+++ src/main/java/helper/UploadHelper.java
@@ -28,7 +28,7 @@ public class UploadHelper {
         for (String filename : contentDisposition) {
             if ((filename.trim().startsWith("filename"))) {
                 String[] name = filename.split("=");
-                String finalFileName = name[1].trim().replaceAll("\"", "");
+                String finalFileName = name[1].trim().replaceAll("\"", "").replaceAll(" ", "_").toLowerCase();
                 return finalFileName;
             }
         }
