955dfdb9c29d694c047b67455f7055cec2a1e858
felipemoura <felipemouracv@gmail.com>
2017-06-19 19:52:36 -0300

    Uploads no contexto da aplicação

--- src/main/java/api/ApplicationResource.java
+++ src/main/java/api/ApplicationResource.java
@@ -3,6 +3,7 @@ package api;
 import javax.annotation.PostConstruct;
 import javax.servlet.ServletContext;
 import javax.ws.rs.core.Context;
+import java.io.File;
 
 /**
  * Created by felipemoura on 18/06/2017.
@@ -14,6 +15,6 @@ public class ApplicationResource {
 
     @PostConstruct
     protected void PostConstruct(){
-        this.UPLOADED_FILE_PATH = this.context.getInitParameter("upload_path");
+        this.UPLOADED_FILE_PATH = context.getRealPath("/uploads_carx//") + File.separator ;
     }
 }
