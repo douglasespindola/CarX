d2e438b950c38a1e42cd3d8fb49e61f344a8b2b3
Douglas Soares Espíndola <dsoares.espindola@gmail.com>
2017-06-24 18:44:09 -0300

    Trata exceções

--- src/main/java/helper/ParseUrlQueryHelper.java
+++ src/main/java/helper/ParseUrlQueryHelper.java
@@ -33,72 +33,81 @@ public class ParseUrlQueryHelper {
         this.cons = cons;
     }
 
-    public String getConsSQL () {
+    public String getConsSQL() {
         //title_ads,like,'corsa'
         String cons = "";
-        if (this.cons.equals("null")){
+        if (this.cons.equals("null")) {
             return cons;
         }
-        if (this.cons.indexOf(":")>0){
+        if (this.cons.indexOf(":") > 0) {
             String[] splitCons = this.cons.split(":");
             int i = 0;
-            for (String c:splitCons){
+            for (String c : splitCons) {
                 String[] value = c.split(",");
                 //cons += cons + value[0] + value[1] + value [2];
-                if (i != splitCons.length - 1){
-                    cons += this.normalizeWhere (value[0], value[1], value [2]) + " and ";
-                }else{
-                    cons += this.normalizeWhere (value[0], value[1], value [2]);
+                if (splitCons.length != 3) {
+                    cons += "";
+                } else if (i != splitCons.length - 1) {
+                    cons += this.normalizeWhere(value[0], value[1], value[2]) + " and ";
+                } else {
+                    cons += this.normalizeWhere(value[0], value[1], value[2]);
                 }
                 i++;
             }
-        }else{
+        } else {
             String[] value = this.cons.split(",");
-            cons += this.normalizeWhere (value[0], value[1], value [2]);
-        }if (!cons.equals("")){
+            if (value.length == 3) {
+                cons += this.normalizeWhere(value[0], value[1], value[2]);
+            } else {
+                cons += "";
+            }
+
+        }
+        if (!cons.equals("")) {
             return "where " + cons;
         }
         return cons;
     }
 
-    public String normalizeWhere(String field, String operator, String value){
+    public String normalizeWhere(String field, String operator, String value) {
         String cons = "";
-        if (operator.equals("like")){
-            return field + " " + operator + " " + "'%"  + value + "%'";
+        if (operator.equals("like")) {
+            return field + " " + operator + " " + "'%" + value + "%'";
         }
-        return field + " " + operator + " "  + this.normalizeValue(value);
+        return field + " " + operator + " " + this.normalizeValue(value);
     }
 
-    public String normalizeValue (String value){
+    public String normalizeValue(String value) {
         try {
-            double d= Double.valueOf(value);
-            if (d == (int)d) {
+            double d = Double.valueOf(value);
+            if (d == (int) d) {
                 return value;
             }
             return value;
-        }catch (Exception e){
-         return "'" + value + "'";
+        } catch (Exception e) {
+            return "'" + value + "'";
         }
     }
 
-    public String getOrderSQL () {
+    public String getOrderSQL() {
         String order = "";
-        if (this.order.equals("null")) {
+        if (this.order.equals("null") || this.order.length() != 2) {
             return order;
+        }else{
+            String[] splitOrder = this.order.split(",");
+            return "order by " + splitOrder[0] + " " + splitOrder[1];
         }
-        String[] splitOrder = this.order.split(",");
-        return "order by " + splitOrder[0] + " " + splitOrder[1];
     }
 
-    public String getLimitSQL () {
+    public String getLimitSQL() {
         String limit = "";
-        if (this.limit.equals("null")) {
+        if (this.limit.equals("null") || this.order.length() != 2) {
             return limit;
         }
         return "limit " + this.limit;
     }
 
-    public String getSQL(){
+    public String getSQL() {
         return this.getConsSQL() + " " + this.getOrderSQL() + " " + this.getLimitSQL();
     }
 }
