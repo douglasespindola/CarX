48424b5f6712be6b46fc3ef1a2006922eb9d9a9e
felipemoura <felipemouracv@gmail.com>
2017-06-25 16:17:02 -0300

    DTO's com mais atributos

--- src/main/java/dto/TokenDto.java
+++ src/main/java/dto/TokenDto.java
@@ -7,6 +7,26 @@ public class TokenDto {
     
     private String token;
 
+    private Integer user_id;
+
+    private String name;
+
+    public Integer getUserId() {
+        return user_id;
+    }
+
+    public void setUserId(Integer user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getToken() {
         return token;
     }
