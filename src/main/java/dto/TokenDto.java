a2735771d7d996cb28d179331e43dba00f4496a6
felipemoura <felipemouracv@gmail.com>
2017-06-27 01:54:54 -0300

    Dto com mais atributos

--- src/main/java/dto/TokenDto.java
+++ src/main/java/dto/TokenDto.java
@@ -11,6 +11,26 @@ public class TokenDto {
 
     private String name;
 
+    private String email;
+
+    private String cpf;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(Long cpf) {
+        this.cpf = cpf.toString();
+    }
+
     public Integer getUserId() {
         return user_id;
     }
